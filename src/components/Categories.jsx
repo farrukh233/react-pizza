d5308e1ed8414a4f4cac9196f6175250b8a2b565
farrukh <farrukhx50@gmail.com>
2022-06-05 00:38:47 +0500

    init: add project

---
+++ src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетереанские",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const onClickCategory = index => {
+    setActiveIndex(index);
+  };
+  return (
+    <div className='categories'>
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            key={i}
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? "active" : ""}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
