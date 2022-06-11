cc0affca2413e2fccb862bca9e93893082b17a15
farrukh <farrukhx50@gmail.com>
2022-06-12 01:33:06 +0500

    feat: add adaptive and cart layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,12 +10,11 @@ function App() {
     <div className='wrapper'>
       <Header />
       <div className='content'>
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
       </div>
     </div>
   );
